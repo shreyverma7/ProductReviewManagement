5503905e3a40d912278241f6112574ebd0e490b3
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-30 10:41:00 +0530

    [Shrey] Add : Abiltiy to add reiveww

---
+++ ProductReviewManagement/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Product
+    {
+        public int ProductID{get; set; }
+        public int UserID { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set;}
+
+       
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,138 @@
+﻿using ProductReviewManagement;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Product review Management");
+        List<Product> list = new List<Product>();
+        list.Add(new Product()
+        {
+            ProductID = 1,
+            UserID = 1,
+            Rating =5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 1,
+            UserID = 2,
+            Rating = 4,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 1,
+            UserID = 3,
+            Rating = 3,
+            Review = "Avg",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 1,
+            UserID = 4,
+            Rating = 1,
+            Review = "bad",
+            isLike = false,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 2,
+            UserID = 1,
+            Rating = 1,
+            Review = "bad",
+            isLike = false,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 2,
+            UserID = 2,
+            Rating = 3,
+            Review = "Avg",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 2,
+            UserID = 3,
+            Rating = 4,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 2,
+            UserID = 5,
+            Rating = 5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 3,
+            UserID = 5,
+            Rating = 5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 3,
+            UserID = 4,
+            Rating = 5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 3,
+            UserID = 3,
+            Rating = 3,
+            Review = "avg",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 3,
+            UserID = 2,
+            Rating = 1,
+            Review = "bad",
+            isLike = false,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 4,
+            UserID = 1,
+            Rating = 5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 4,
+            UserID = 2,
+            Rating = 5,
+            Review = "Good",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 4,
+            UserID = 3,
+            Rating = 3,
+            Review = "avg",
+            isLike = true,
+        });
+        list.Add(new Product()
+        {
+            ProductID = 4,
+            UserID = 4,
+            Rating = 1,
+            Review = "bad",
+            isLike = false,
+        });
+    }
+}
